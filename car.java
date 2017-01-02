29252edd9450e089afe393449ad75f15864be78e
inukai <hoge@hoge.com>
2017-01-02 17:30:56 +0900

    separate source code

---
+++ car.java
@@ -0,0 +1,136 @@
+
+class Car {
+	int id, speed = 1;
+	int[] dep = new int[2];
+	int[] arr = new int[2];
+	int[] position = new int[2];
+	int[][] route = new int[6][];
+	int phase = 0; // 経路の何段階目か
+	int genStep = 0; // 何step目に生成されるか
+	boolean isGenerated = false;
+	boolean isArrived = false;
+
+	// 測定に用いる変数
+	boolean expedSec = false; //1つ目の交差点を通過したか
+	int waitingLength1 = 0; // 1つ目の交差点を通過するまでの停車時間
+	int waitingLength2 = 0; // 目的地に到着するまでの停車時間
+
+	public Car(int id, int genStep) {
+		this.id = id;
+		this.genStep = genStep;
+		this.genRoute();
+	}
+
+	// 次のセルの車の存否を確かめた上で進行
+	void right() { position[0] += speed; }
+	void left()  { position[0] -= speed; }
+	void up()    { position[1] += speed; }
+	void down()  { position[1] -= speed; }
+
+	// 交差点に居るか否かの判定
+	boolean isInSec() {
+		if(position[1]==20 && (position[0]==20 || position[0]==40 || position[0]==60))
+			return true;
+		else	return false;
+	}
+
+	// 目的地に向かって自動で進む
+	// 1stepだけで出来ることをやる
+	ReturnValue go() {
+		ReturnValue v = new ReturnValue();
+
+		// nextX, nextYは次に向かうべき交差点の場所
+		int nextX = route[phase+1][0], nextY = route[phase+1][1];
+
+		if      (position[0] < nextX){ v.direction="right"; right();}
+		else if (position[0] > nextX){ v.direction="left";  left();}
+		else if (position[1] < nextY){ v.direction="up";    up();  }
+		else if (position[1] > nextY){ v.direction="down";  down();}
+		else System.out.println("something wrong");
+
+		// 交差点に達したらphaseをカウントアップ
+		if (route[phase+1][0] == position[0] && route[phase+1][1] == position[1]) phase++;
+		if (arr[0] == position[0] && arr[1] == position[1]) isArrived = true;
+
+		v.x = position[0];
+		v.y = position[1];
+
+		return v;
+	}
+
+	ReturnValue goCheck() {
+		ReturnValue v = new ReturnValue();
+
+		// nextX, nextYは次に向かうべき交差点の場所
+		int nextX = route[phase+1][0], nextY = route[phase+1][1];
+
+		if (position[0] == nextX){
+			v.x = position[0];
+			v.y = (position[1] > nextY) ? position[1]-1 : position[1]+1;
+			v.direction = (position[1] > nextY) ? "down" : "up";
+		}
+		else if (position[1] == nextY){
+			v.x = (position[0] > nextX) ? position[0]-1 : position[0]+1;
+			v.y = position[1];
+			v.direction = (position[0] > nextY) ? "left" : "right";
+		}
+		else System.out.println("something wrong");
+
+		return v;
+	}
+
+	void genRoute() {
+
+		int point[][] = {
+			// 起終点となりうる場所は次の8つのみ
+			{ 0,20}, {80,20},
+			{20, 0}, {40, 0},
+			{60, 0}, {20,40},
+			{40,40}, {60,40}
+		};
+
+		// 起点p1と終点p2を決定する(選ぶ)
+		Random r = new Random();
+		int p1 = r.nextInt(8);
+		int p2 = r.nextInt(8);
+		while (p1 == p2) p2 = r.nextInt(8);
+		this.dep = this.position = point[p1];
+		this.arr = point[p2];
+
+		// ルートの生成
+		this.route[0] = this.dep;
+		int i=1, x, y;
+		boolean flag = true;
+
+		while(flag) {
+			x = route[i-1][0];
+			y = route[i-1][1];
+			route[i] = new int[2];
+
+			// 上下に進むだけの場合
+			if (x == arr[0]){
+				route[i][0] = x;
+				route[i][1] = (y < arr[1]) ? y+20 : y-20;
+			}
+			// 左右に進まなければならない場合
+			else if (y==20){
+				route[i][0] = (x < arr[0]) ? x+20 : x-20;
+				route[i][1] = y;
+			} else{
+				route[i][0] = x;
+				route[i][1] = 20;
+			}
+			//System.out.printf("route[%d]に%d,%dを追加しました。\n",i,route[i][0],route[i][1]);
+
+			flag = route[i][0]!=arr[0] || route[i][1]!=arr[1];
+			i++;
+		}
+	}
+	// genRoute()終了
+
+	void printLocation() {
+		System.out.printf("[%3d]:(%2d,%2d,%2d,%2d) ", id, position[0], position[1], waitingLength1, waitingLength2);
+		if (id%6 == 5) System.out.println();
+	}
+}
+
---
+++ signal.java
@@ -0,0 +1,74 @@
+class Signal {
+	public double GANMA = 1.5;
+	public double DELTA = 1.1;
+	public double EPSILON = 0.2;
+	public int X = 3; // 右折レーンに溜まれる台数
+	public double P = 0.6;
+	public double Q = 0.5;
+	public double R = 0.3;
+
+	double cap_i = 0.45;
+	double alpha = 0.25 * cap_i; // cap_iが変わったら、あわせて変更
+	double beta  = 0.125* cap_i;
+
+	// 表2に従い，初期値を代入
+	double split0 = 0.5, split1 = 0.5;
+	int cycle = 50, clearance = 5;
+	int offset = 0;
+
+	// 0:clearance，1:第1現示，2:第2現示，3:第1現示右折，4:第2現示右折
+	int status=0;
+	int presentLength=0; // 今の現示が何step続いているか
+	int id;
+	int[] position = new int[2];
+	int[] rightTurnLane = new int[2];
+	int mode = 1; //1:独立、2:起点、3:従属
+	//従属モードにおける起点の信号機の番号。独立or起点のときは-1
+	int parent = -1;
+
+	public Signal(int id, int x, int y) {
+		this.id = id;
+		this.position[0] = x;
+		this.position[1] = y;
+		this.rightTurnLane[0] = this.rightTurnLane[1] = 0;
+	}
+
+	public boolean changeStatus() {
+		// cycle等を検討し、現示を変える必要があるか判断
+		switch (status){
+			case 0: //clearance
+				if(presentLength>=clearance){ //第１現示にする
+					status=1;
+					presentLength=0;
+				}
+			case 1: //第１現示
+
+			case 2:
+
+			case 3:
+
+		}
+		// if pij>beta or pji>beta then suggestOffset
+
+
+		// 現示が変わったらtrueを返す
+		return true;
+	}
+
+	public void suggestOffset(int i) {
+	}
+
+	// 従属モードになるか決める（(2)式）
+	public void considerSuggestion(int step, int e, int cycle, int suggester) {
+		if (step<=EPSILON*cycle || step>=(1-EPSILON)*cycle){
+			this.mode = 3;
+			this.parent = suggester;
+		}
+	}
+
+	public void printStatus() {
+		System.out.printf("[%d] mode:%d, parent:%d, status:%d, presentLength:%d\n", id, mode, parent, status, presentLength);
+
+	}
+}
+
