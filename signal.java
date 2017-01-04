7835ba0a002e929b42aafe2de7aa2e2736a3056f
hogehoge <hogehoge>
2017-01-05 03:02:08 +0900

    readme

--- signal.java
+++ signal.java
@@ -54,6 +54,53 @@ class Signal {
 		time[5] = 0;
 	}
 
+	// サイクル毎のルーチンワーク
+	public void changeStatus() {
+		// cycle等を検討し、現示を変える必要があるか判断
+		if(presentLength >= time[status]){
+			do { status++; if(status==6) status=0; } while(time[status]==0);
+			presentLength=0;
+		}
+
+		if(status==0) reconsider();
+
+		// if pij>beta or pji>beta then suggestOffset
+
+		//最後にインクリメント（0に戻ってもインクリメント）
+		this.presentLength++;
+	}
+
+	public void reconsider(){
+		// 振り出しに戻った(status==0)のでサイクル長の見直しをする
+
+		double lambda = 0.0; // 交差点飽和度：暫定
+		cycle = (int)((1.5 * clearance + 5.0) / (1.0-lambda));
+
+		// 独立モードの現示維持時間(右折なし)
+		time[0] = clearance;
+		time[1] = (int)(cycle*split0)-clearance;
+		time[2] = clearance;
+		time[3] = 0;
+		time[4] = (int)(cycle*split1)-clearance;
+		time[5] = 0;
+	}
+
+	public void suggestOffset(int i) {
+	}
+
+	// 従属モードになるか決める（(2)式）
+	public void considerSuggestion(int step, int e, int cycle, int suggester) {
+		if (step<=EPSILON*cycle || step>=(1-EPSILON)*cycle){
+			this.mode = 3;
+			this.parent = suggester;
+		}
+	}
+
+	public void printStatus() {
+		System.out.printf("[%d] mode:%d, parent:%d, status:%d, presentLength:%d\n", id, mode, parent, status, presentLength);
+
+	}
+
 	// 車に，進んでいいか教えてあげる
 	public boolean isAllowed(String nowDirection, String nextDirection){
 
@@ -76,33 +123,5 @@ class Signal {
 			{ if(status==1) return true; else return false; }
 	}
 
-	public void changeStatus() {
-		// cycle等を検討し、現示を変える必要があるか判断
-		if(presentLength >= time[status]){
-			do { status++; if(status==6) status=0; } while(time[status]==0);
-			presentLength=0;
-		}
-
-		// if pij>beta or pji>beta then suggestOffset
-
-		//最後にインクリメント（0に戻ってもインクリメント）
-		this.presentLength++;
-	}
-
-	public void suggestOffset(int i) {
-	}
-
-	// 従属モードになるか決める（(2)式）
-	public void considerSuggestion(int step, int e, int cycle, int suggester) {
-		if (step<=EPSILON*cycle || step>=(1-EPSILON)*cycle){
-			this.mode = 3;
-			this.parent = suggester;
-		}
-	}
-
-	public void printStatus() {
-		System.out.printf("[%d] mode:%d, parent:%d, status:%d, presentLength:%d\n", id, mode, parent, status, presentLength);
-
-	}
 }
 
